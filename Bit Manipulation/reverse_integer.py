1ef027f4207a5f1313a1891e7a0ade6a7dacbe0a
ShevinuM <nsmcooray@mun.ca>
2023-05-03 13:04:58 +0530

    reverse_integer completed

---
+++ Bit Manipulation/reverse_integer.py
@@ -0,0 +1,50 @@
+import math
+
+class Solution:
+    # This was my initial solution to this problem but I realized this approach was too slow because of O(n) time complexity
+    def reverse1(x: int) -> int:
+        if x < 0:
+            negative = True
+            x = -x
+        else:
+            negative = False
+        string = str(x)
+        new_string = ""
+        for character in string:
+            new_string = character + new_string
+        return_int = -int(new_string) if negative else int(new_string)
+        if return_int >= -(2**31) and return_int <= (2**31) - 1:
+            return return_int
+        else:
+            return 0
+    
+    # This is my second approach
+    def reverse2(x: int) -> int:
+        l = abs(x)
+        r = abs(x) // 10 ** (len(str(abs(x))) - 1)
+        val = 0
+        while l > 9:
+            val = (((l%10)/10)+val)*10
+            l = l//10
+            if val > 214748364:
+                return 0
+        if val == (214748364) and abs(l) > 7:
+                return 0
+        return int(val*10 + r) if x > 0 else int(-(val*10 + r))
+    
+class Test:
+    print()
+    print("Test for my second approach")
+    print("____________________________")
+    print("1234 -> {}".format(Solution.reverse2(1234)))
+    print()
+    print("-123 -> {}".format(Solution.reverse2(-123)))
+    print()
+    print("120 -> {}".format(Solution.reverse2(120)))
+    print()
+    print("-1463847412 -> {}".format(Solution.reverse2(-1463847412)))
+    print()
+
+Test()
+
+    
