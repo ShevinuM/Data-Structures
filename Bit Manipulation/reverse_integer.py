45f8a09ba1ef8750494183866f764dc399dc7b70
ShevinuM <nsmcooray@mun.ca>
2023-05-03 13:08:42 +0530

    Update reverse_integer.py

--- Bit Manipulation/reverse_integer.py
+++ Bit Manipulation/reverse_integer.py
@@ -1,7 +1,7 @@
 import math
 
 class Solution:
-    # This was my initial solution to this problem but I realized this approach was too slow because of O(n) time complexity
+    # This was my initial solution to this problem using strings
     def reverse1(x: int) -> int:
         if x < 0:
             negative = True
@@ -34,8 +34,8 @@ class Solution:
     
 class Test:
     print()
-    print("Test for my second approach")
-    print("____________________________")
+    print("Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the \nsigned 32-bit integer range [-231, 231 - 1], then return 0. Assume the environment does not allow you to store 64-bit \nintegers (signed or unsigned).")
+    print("_____________________________________________________________________________________________________________________")
     print("1234 -> {}".format(Solution.reverse2(1234)))
     print()
     print("-123 -> {}".format(Solution.reverse2(-123)))
