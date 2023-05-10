fafe3095f9af28b8de279688ee87f7c06e154662
ShevinuM <nsmcooray@mun.ca>
2023-05-10 23:49:49 +0530

    Update inorder_traversal.py
    Added inorder traversal using Iteration

--- Trees/inorder_traversal.py
+++ Trees/inorder_traversal.py
@@ -1,9 +1,20 @@
 '''
-
 Given the root of a binary tree, return the inorder traversal of its nodes' values.
-
 '''
 
-# Initial solution using recursion only requires 1 line of code
+# Initial solution using recursion. Only requires 1 line of code
 def inorder(root): return inorder(root.left) + [root.val] + inorder(root.right) if root else []
 
+# Alternative solution using iteration
+def inorderTraversal(root):
+        stack = []
+        curr = root
+        res = []
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            res.append(curr.val)
+            curr = curr.right
+        return res
