1b95d9349e0090603b3624def1ba9d13fae0d01a
ShevinuM <nsmcooray@mun.ca>
2023-05-16 21:45:54 +0530

    Update Linked Lists folder

--- Linked Lists/add_two_numbers.py
+++ Linked Lists/add_two_numbers.py
@@ -44,3 +44,28 @@ class Solution():
         curr.next = ListNode(result) if result else None
 
         return return_list.next
+
+
+# Print linked-list
+def print_linked_list(list):
+    while list:
+        print(list.val, end="->") if list.next is not None else print(list.val)
+        list = list.next
+
+# Case 1
+# Create l1
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+# Create l2
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+# add Two Numbers
+solution = Solution.addTwoNumbers(l1,l2)
+print_linked_list(solution)
+
+# Case 2
+l1 = ListNode(0)
+l2 = ListNode(0)
+print_linked_list(Solution.addTwoNumbers(l1,l2))
