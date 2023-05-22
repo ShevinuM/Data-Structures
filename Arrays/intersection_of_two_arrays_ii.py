63665ac18834460dc14da9f47c50a9f351f99b78
ShevinuM <nsmcooray@mun.ca>
2023-05-22 18:10:03 +0530

    Update intersection_of_two_arrays_ii.py

--- Arrays/intersection_of_two_arrays_ii.py
+++ Arrays/intersection_of_two_arrays_ii.py
@@ -17,3 +17,17 @@ class Solution:
                     nums1_map[elementB] = nums1_map.get(elementB) - 1
                     break
         return res
+
+    def test(self, nums1, nums2):
+        print("Input: nums1 = {0}, nums2 = {1}, Output: {2}".format(nums1, nums2, self.intersect(nums1, nums2)))
+
+sol = Solution()
+sol.test([1,2,2,1], [2,2])
+sol.test([4,9,5], [9,4,9,8,4])
+sol.test([1,2,2,1], [2])
+sol.test([1,2,2,1], [1,2,2,1])
+
+# I didn't know this problem could be solved more simply and efficiently using the & operator on Counter objects. 
+class Solution2:
+    def intersect(self, nums1, nums2):
+        return list((Counter(nums1) & Counter(nums2)).elements())
