b01cda0a0df4ffe7929aae5de5aa7902d90227a2
ShevinuM <nsmcooray@mun.ca>
2023-05-30 17:33:39 +0530

    Update binary_tree_level_order_traversal.py

--- Trees/binary_tree_level_order_traversal.py
+++ Trees/binary_tree_level_order_traversal.py
@@ -1,3 +1,7 @@
+'''
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+'''
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
